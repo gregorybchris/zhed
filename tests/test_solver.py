980acdb74771a673bd7480067fce207cd2d921da
Chris Gregory <gregorybchris@gmail.com>
2025-07-01 22:34:42 -0700

    Move board fixture to conftest

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,32 +1,5 @@
-import pytest
-
 from zhed.models import Board, Direction, Tile
 from zhed.solver import Solver
-
-
-@pytest.fixture(name="board")
-def board_fixture() -> Board:
-    """
-    ╭─────────────────╮
-    │ Level 5         │
-    ├─────────────────┤
-    │ □ □ □ □ □ □ □ □ │
-    │ □ □ □ □ □ □ □ □ │
-    │ □ □ □ 2 □ □ ◎ □ │
-    │ □ □ □ □ □ □ □ □ │
-    │ □ 2 □ □ □ □ □ □ │
-    │ □ □ 1 □ 2 □ □ □ │
-    │ □ □ □ □ □ □ □ □ │
-    │ □ □ □ □ □ □ □ □ │
-    ╰─────────────────╯
-    """
-    board = Board.new(8, 8)
-    board.set((2, 3), 2)
-    board.set((4, 1), 2)
-    board.set((5, 2), 1)
-    board.set((5, 4), 2)
-    board.set((2, 6), Tile.Goal)
-    return board
 
 
 class TestSolver:
