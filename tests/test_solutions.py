0be487e7bb5ede982b2b0d1ec69a406d3848cba3
Chris Gregory <gregorybchris@gmail.com>
2025-07-07 14:10:51 -0700

    Test solutions solve levels

---
+++ tests/test_solutions.py
@@ -0,0 +1,24 @@
+import logging
+
+from zhed.loading import get_solution, load_levels
+from zhed.mover import Mover
+
+logger = logging.getLogger(__name__)
+
+
+class TestSolutions:
+    def test_solutions(self) -> None:
+        levels = load_levels()
+
+        n_tested = 0
+        for level in levels:
+            solution = get_solution(level.number)
+
+            if solution is None:
+                continue
+
+            board = level.get_board()
+            has_won, _ = Mover.make_moves(board, solution.moves)
+            assert has_won, f"Solution for level {level.number} did not result in a win"
+            n_tested += 1
+        logger.info(f"Tested {n_tested} solutions")
